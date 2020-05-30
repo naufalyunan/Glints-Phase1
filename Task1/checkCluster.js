6778f442635d70489fe6aa2cb2a89de11bd9dca9
naufalyunan <naufalyunan45@gmail.com>
2020-05-30 18:12:25 +0700

    finish main function and add explanation

--- Task1/checkCluster.js
+++ Task1/checkCluster.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 const inputFilePath = './' + process.argv[2]
 if(!process.argv[2]) {
 	console.log('You need to enter your input file name')
-} else if(process.argv[2] != 'input1.txt' && process.argv[2]) {
+} else if(process.argv[2] != 'input1.txt' && process.argv[2] != 'input2.txt') {
 	console.log('You need to enter the correct file name')
 } else {
 	const input = fs.readFileSync(inputFilePath, 'utf8')
@@ -17,6 +17,11 @@ if(!process.argv[2]) {
 }
 
 function checkNeighbour (arr, i, j, count) {
+	/*
+	check the elements around the [i, j] element if it is not a 0,
+	increment counter, put it in dictionary, turn that element to 0
+	and set condition to true. do this until all elements around [i, j] is checked
+	*/
 	let condition = false
 	let dict = {}
 	dict[[i, j]] = true
@@ -39,9 +44,39 @@ function checkNeighbour (arr, i, j, count) {
 			}
 		}
 	}
+	/*
+	if  condition is true recurse the function in [m, n position] with
+	the updated array. if false, then return the counter
+	*/
 	if(condition) {
 		return checkNeighbour(arr, m, n, count)
 	} else {
 		return count++
 	}
+}
+
+function demolition(str) {
+	/*
+	change input string into array
+	*/
+	let inputArr = str.split('\n')
+	inputArr.forEach((el, i, arr) => {
+		let temp = el.split('')
+		arr[i] = temp
+	})
+	/*
+	call checkNeighbour for each element in the input, if count > 1 
+	increment the distinct cluster
+	*/
+	let output = 0
+	for(let i = 0; i < inputArr.length; i++) {
+		for(let j = 0; j < inputArr[i].length; j++) {
+			let count = 0
+			count = checkNeighbour(inputArr, i, j ,count)
+			if(count > 1) {
+				output++
+			}
+		}
+	}
+	return output
 }
